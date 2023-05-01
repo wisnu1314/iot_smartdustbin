82f4c37ca47360d0ea88d2854ea670c773936638
Wisnu Aditya <61625865+wisnu1314@users.noreply.github.com>
2023-05-01 15:24:06 +0700

    Dummy Dashboard

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,118 +1,186 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import React from "react";
+import {
+	Box,
+	Text,
+	NumberInput,
+	Input,
+	Button,
+	NumberInputField,
+} from "@chakra-ui/react";
+import {
+	ResponsiveContainer,
+	LineChart,
+	CartesianGrid,
+	XAxis,
+	YAxis,
+	Tooltip,
+	Legend,
+	Line,
+} from "recharts";
+import { Inter } from "next/font/google";
+import { useEffect, useState } from "react";
 
-const inter = Inter({ subsets: ['latin'] })
-
-export default function Home() {
-  return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+const inter = Inter({ subsets: ["latin"] });
+const data = [
+	{
+		name: "Page A",
+		uv: 4000,
+		pv: 2400,
+		amt: 2400,
+	},
+	{
+		name: "Page B",
+		uv: 3000,
+		pv: 1398,
+		amt: 2210,
+	},
+	{
+		name: "Page C",
+		uv: 2000,
+		pv: 9800,
+		amt: 2290,
+	},
+	{
+		name: "Page D",
+		uv: 2780,
+		pv: 3908,
+		amt: 2000,
+	},
+	{
+		name: "Page E",
+		uv: 1890,
+		pv: 4800,
+		amt: 2181,
+	},
+	{
+		name: "Page F",
+		uv: 2390,
+		pv: 3800,
+		amt: 2500,
+	},
+	{
+		name: "Page G",
+		uv: 3490,
+		pv: 4300,
+		amt: 2100,
+	},
+];
+const Dashboard = () => {
+	const [mqttInput, setMqttInput] = useState("30000");
+	return (
+		<main
+			className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+			style={{ background: "white" }}
+		>
+			<Box
+				display="flex"
+				flexDirection="row"
+				justifyContent="space-between"
+				border="1vh solid"
+				borderColor="black"
+				borderRadius={20}
+				backgroundColor="white"
+				minW="100%"
+				minH="100%"
+				w="800px"
+				h="800px"
+				alignItems="center"
+				bottom={0}
+				lineHeight="100%"
+				gap={10}
+				paddingX={4}
+			>
+				<ResponsiveContainer width="30%" height="50%">
+					<LineChart
+						margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+						data={data}
+					>
+						<CartesianGrid strokeDasharray="3 3" />
+						<XAxis dataKey="name" />
+						<YAxis />
+						<Tooltip />
+						<Legend />
+						<Line type="monotone" dataKey="pv" stroke="#8884d8" />
+						<Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+					</LineChart>
+				</ResponsiveContainer>
+				<ResponsiveContainer width="30%" height="50%">
+					<LineChart
+						margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+						data={data}
+					>
+						<CartesianGrid strokeDasharray="3 3" />
+						<XAxis dataKey="name" />
+						<YAxis />
+						<Tooltip />
+						<Legend />
+						<Line type="monotone" dataKey="pv" stroke="#8884d8" />
+						<Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+					</LineChart>
+				</ResponsiveContainer>
+				<Box
+					display="flex"
+					alignItems="center"
+					justifyContent="center"
+					flexDirection="column"
+					gap={20}
+					w="20%"
+					h="95%"
+					border="0.5vh solid"
+					borderColor="black"
+					borderRadius={20}
+					background="green"
+				></Box>
+				<Box
+					display="flex"
+					alignItems="center"
+					justifyContent="center"
+					flexDirection="column"
+					gap={20}
+					w="20%"
+					h="95%"
+					border="0.5vh solid"
+					borderColor="black"
+					borderRadius={20}
+					background="green"
+				>
+					<Text
+						fontFamily={inter.className}
+						textColor="black"
+						fontWeight="bold"
+						fontSize={20}
+					>
+						MQTT Period
+					</Text>
+					<Input
+						value={mqttInput}
+						type="number"
+						min={1000}
+						onChange={(e) => setMqttInput(e.target.value)}
+						placeholder="30000"
+						size="lg"
+						textColor="black"
+						w="95%"
+					/>
+					<Button
+						background="yellow"
+						border="5px solid"
+						borderColor="black"
+						borderRadius={20}
+						w="95%"
+						h="30px"
+					>
+						<Text
+							fontFamily={inter.className}
+							textColor="black"
+							fontWeight="bold"
+						>
+							Submit
+						</Text>
+					</Button>
+				</Box>
+			</Box>
+		</main>
+	);
+};
+export default Dashboard;
